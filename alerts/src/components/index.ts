46d208be58121576cba9b1d64f5da036852c2dcf
melserngl <128845117+nglmercer@users.noreply.github.com>
2026-03-27 12:30:25 -0500

    remove all

--- alerts/src/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { Canvas } from './Canvas';
-export { Sidebar } from './Sidebar';
-export { PropertiesPanel } from './PropertiesPanel';
-export { PlaybackControls } from './PlaybackControls';
--- alerts/src/index.tsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from 'react-dom/client';
-import AlertEditor from './AlertEditor';
-import './index.css';
-
-const root = document.getElementById('root');
-if (root) {
-  createRoot(root).render(<AlertEditor />);
-}
