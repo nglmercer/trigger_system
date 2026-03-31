22b72028abfc325038544d7aafad48d02c74794b
melserngl <128845117+nglmercer@users.noreply.github.com>
2026-03-30 23:22:10 -0500

    delete editor

--- trigger-alerts/index.ts
+++
@@ -1,4 +0,0 @@
-export { AlertBuilder,createAlert } from './src/builder/AlertBuilder';
-export { TriggerAlert } from './src/components/TriggerAlert';
-export { AlertExporter, AlertStyleExporter } from './src/exporter';
-export * from './src/styles/types';
